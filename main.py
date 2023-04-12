6f0a46b558a2d744bc5dd4c51f0f0bce0d25ce4f
AidenVu1 <100857445+AidenVu1@users.noreply.github.com>
2023-04-12 14:24:52 +0000

    update

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-# Define the cost of each pizza size
 LARGE_PIZZA_COST = 6.00
 XL_PIZZA_COST = 10.00
 
-# Define the cost of each topping option
 ONE_TOPPING_COST = 1.00
 TWO_TOPPINGS_COST = 1.75
 THREE_TOPPINGS_COST = 2.50
 FOUR_TOPPINGS_COST = 3.35
 
-# Define the tax rate
 HST_RATE = 0.13
 
-# Prompt the user for pizza size
 pizza_size = input("Enter 'L' for large pizza or 'XL' for extra large pizza: ")
 
-# Prompt the user for the number of toppings
-num_toppings = int(input("Enter the number of toppings (1-4): "))
-
-# Calculate the pizza cost based on size
 if pizza_size == "L":
     pizza_cost = LARGE_PIZZA_COST
 elif pizza_size == "XL":
@@ -26,7 +18,16 @@ else:
     print("Invalid pizza size")
     exit()
 
-# Calculate the cost of toppings based on number
+while True:
+    try:
+        num_toppings = int(input("Enter the number of toppings (1-4): "))
+        if num_toppings >= 1 and num_toppings <= 4:
+            break
+        else:
+            print("Invalid number of toppings, please try again.")
+    except ValueError:
+        print("Invalid input, please enter a number between 1 and 4.")
+
 if num_toppings == 1:
     toppings_cost = ONE_TOPPING_COST
 elif num_toppings == 2:
@@ -39,14 +40,11 @@ else:
     print("Invalid number of toppings")
     exit()
 
-# Calculate the subtotal and tax
 subtotal = pizza_cost + (num_toppings * toppings_cost)
 tax = subtotal * HST_RATE
 
-# Calculate the total cost
 total = subtotal + tax
 
-# Display the results
 print(f"Subtotal: ${subtotal:.2f}")
 print(f"Tax: ${tax:.2f}")
 print(f"Total: ${total:.2f}")
